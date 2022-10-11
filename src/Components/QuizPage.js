fd2cab9f32ab02f9070f4a8e1fcbf5d090e19557
saidrafi11 <said.rafi@outlook.com>
2022-10-11 23:31:31 +0600

    cleaned code

--- src/Components/QuizPage.js
+++ src/Components/QuizPage.js
@@ -11,12 +11,12 @@ const QuizPage = () => {
     return (
         <div className='container justify-content-center col-lg-6 col-md-6 col-sm-12'>
             <section className='text-center'>
-            <h1 className='text-align-center text-info'>Here are the questions</h1>
-            <h3 className='text-align-center'>Select the best option</h3>
+                <h1 className='text-align-center text-info'>Here are the questions</h1>
+                <h3 className='text-align-center'>Select the best option</h3>
             </section>
-            
+
             {
-                questions.map(questionObj=><QCard key={questionObj.id} questionObj={questionObj}></QCard>)
+                questions.map(questionObj => <QCard key={questionObj.id} questionObj={questionObj}></QCard>)
             }
         </div>
     );
--- src/Components/Stat.js
+++ src/Components/Stat.js
@@ -7,25 +7,25 @@ const Stat = () => {
     const quiztopics = quiztopicsRaw.data;
     console.log(quiztopics)
     return (
-        
+
         <div className='container justify-content-center col-lg-6 col-md-6 col-sm-12'>
-                <section className='text-center m-5'>
-            <h1 className='text-align-center text-info'>Statistics</h1>
-            
+            <section className='text-center m-5'>
+                <h1 className='text-align-center text-info'>Statistics</h1>
+
             </section>
 
             <div className='container'>
-            <ResponsiveContainer width="95%" height={400}>       
-            <BarChart width={500} height={250} data={quiztopics}>
-  <CartesianGrid strokeDasharray="3 3" />
-  <XAxis dataKey="name" />
-  <YAxis />
-  <Tooltip />
-  <Legend />
-  <Bar dataKey="total" fill="#8884d8" />
-  
-</BarChart>
-</ResponsiveContainer>
+                <ResponsiveContainer width="95%" height={400}>
+                    <BarChart width={500} height={250} data={quiztopics}>
+                        <CartesianGrid strokeDasharray="3 3" />
+                        <XAxis dataKey="name" />
+                        <YAxis />
+                        <Tooltip />
+                        <Legend />
+                        <Bar dataKey="total" fill="#8884d8" />
+
+                    </BarChart>
+                </ResponsiveContainer>
             </div>
         </div>
     );
